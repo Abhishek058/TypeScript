36966eb54437c49949c7b04637e9be2c5f590493
Abhishek058 <abhishek0058.be21@chitkara.edu.in>
2023-09-25 14:15:49 +0530

    objects in lecture 7 & readonly, '?' keywords in lecture 8

---
+++ Lecture_08/Lecture_08.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var myUser = {
+  id: "1qw23e",
+  name: "John",
+  email: "j@j.mail",
+  isActive: true,
+  // creditCart: 1234567890, //can pass or may be  not
+};
+myUser.email = "j@bj.mail";
---
+++ Lecture_08/Lecture_08.ts
@@ -0,0 +1,22 @@
+type User = {
+  readonly id: string;
+  name: string;
+  email: string;
+  isActive: boolean;
+  creditCart?: number; // ? -> use to set the field opetional
+};
+
+let myUser: User = {
+  id: "1qw23e",
+  name: "John",
+  email: "j@j.mail",
+  isActive: true,
+
+  // creditCart: 1234567890, //can pass or may be  not
+};
+
+myUser.email = "j@bj.mail";
+
+// myUser.id = "21233"; //readonly variable cant be changed
+
+export {};
